763c99f719218ca1c692c409512af8eb29d15dad
Sergey Kirichenko <spkirich@gmail.com>
2022-09-19 20:52:30 +0300

    Fix task-01.c

--- lab-02/task-01.c
+++ lab-02/task-01.c
@@ -1,59 +1,73 @@
 #include <stdio.h>
-
 #include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 
+#define CHILD_COUNT 3
+
 /*!
- * Код процесса-предка
+ * Код родительского процесса
+ *
+ * \param children - массив идентификаторов дочерних процессов.
+ * \param count - количество дочерних процессов.
  */
 
-void parent(int child1_pid, int child2_pid)
+void parent(pid_t *children, size_t count)
 {
-    while (1)
-        printf("I am %d, my group is %d, my children are %d and %d.\n",
-            getpid(), getpgrp(), child1_pid, child2_pid);
+    printf("I am %d; my group is %d; ",
+        getpid(), getpgrp());
+
+    if (count == 0)
+        printf("I have no children.\n");
+
+    else
+    {
+        printf("my children are ");
+
+        for (size_t i = 0; i < count - 1; i++)
+            printf("%d, ", children[i]);
+
+        printf("%d.\n", children[count - 1]);
+    }
 }
 
 /*!
- * Код процесса-потомка
+ * Код дочернего процесса
  */
 
 void child()
 {
-    while (1)
-        printf("I am %d, my group is %d, my parent is %d.\n",
-            getpid(), getpgrp(), getppid());
+    printf("I am %d; my group is %d; my parent is %d.\n",
+        getpid(), getpgrp(), getppid());
+
+    sleep(1);
+
+    printf("I am %d; my group is %d; my parent is %d.\n",
+        getpid(), getpgrp(), getppid());
 }
 
 int main()
 {
-    int child1_pid;
+    int children[CHILD_COUNT];
 
-    if ((child1_pid = fork()) == -1)
+    for (size_t i = 0; i < CHILD_COUNT; i++)
     {
-        perror("Failed to fork");
-        exit(EXIT_FAILURE);
-    }
-
-    else if (child1_pid == 0)
-        child();
-
-    else
-    {
-        int child2_pid;
-
-        if ((child2_pid = fork()) == -1)
+        if ((children[i] = fork()) == -1)
         {
-            perror("Failed to fork!");
-            exit(EXIT_FAILURE);
+            perror("Failed to fork");
+            exit(1);
         }
 
-        else if (child2_pid == 0)
+        // Дочерний процесс
+        else if (children[i] == 0)
+        {
             child();
-
-        else
-            parent(child1_pid, child2_pid);
+            exit(0);
+        }
     }
+
+    // Родительский процесс
+    parent(children, CHILD_COUNT);
 
     return 0;
 }
