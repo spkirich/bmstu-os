3e26ec788216b79aea866fa33317e6a252f8ac09
Sergey Kirichenko <spkirich@gmail.com>
2022-10-08 10:39:03 +0300

    Disfigure task-02.c

--- lab-02/task-02.c
+++ lab-02/task-02.c
@@ -1,87 +1,50 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define CHILD_COUNT 3
-
-/*!
- * Код родительского процесса
- *
- * \param children - массив идентификаторов дочерних процессов.
- * \param count - количество дочерних процессов.
- */
-
-void parent(const pid_t *children, size_t count)
+int main()
 {
+    int child_pid[2];
+
+    for (size_t i = 0; i < 2; i++)
+    {
+        if ((child_pid[i] = fork()) == -1)
+        {
+            perror("Failed to fork");
+            return 1;
+        }
+
+        else if (child_pid[i] == 0)
+        {
+            printf("I am %d; my group is %d; my parent is %d.\n",
+                getpid(), getpgrp(), getppid());
+
+            sleep(1);
+
+            return 0;
+        }
+    }
+
     printf("I am %d; my group is %d; ",
         getpid(), getpgrp());
 
-    if (count == 0)
-        printf("I have no children.\n");
+    printf("my children are %d and %d\n",
+        child_pid[0], child_pid[1]);
 
-    else
+    int stat;
+
+    for (size_t i = 0; i < 2; i++)
     {
-        printf("my children are ");
+        pid_t child = wait(&stat);
 
-        for (size_t i = 0; i < count - 1; i++)
-            printf("%d, ", children[i]);
+        printf("My child %d has finished ", child);
 
-        printf("%d.\n", children[count - 1]);
+        if (WIFEXITED(stat))
+            printf("with return code %d.\n", WEXITSTATUS(stat));
 
-        int stat;
-
-        for (size_t i = 0; i < count; i++)
-        {
-            // Ждём дочерний процесс
-            pid_t child = wait(&stat);
-
-            printf("My child %d has finished ", child);
-
-            if (WIFEXITED(stat))
-                printf("with exit code %d.\n", WEXITSTATUS(stat));
-
-            else
-                printf("abnormally.\n");
-        }
+        else
+            printf("abnormally.\n");
     }
-}
-
-/*!
- * Код дочернего процесса
- */
-
-void child()
-{
-    printf("I am %d; my group is %d; my parent is %d.\n",
-        getpid(), getpgrp(), getppid());
-
-    sleep(1);
-}
-
-int main()
-{
-    int children[CHILD_COUNT];
-
-    for (size_t i = 0; i < CHILD_COUNT; i++)
-    {
-        if ((children[i] = fork()) == -1)
-        {
-            perror("Failed to fork");
-            exit(1);
-        }
-
-        // Дочерний процесс
-        else if (children[i] == 0)
-        {
-            child();
-            exit(0);
-        }
-    }
-
-    // Родительский процесс
-    parent(children, CHILD_COUNT);
 
     return 0;
 }
