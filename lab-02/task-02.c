feb5db178bb3429a5056054d50ba62432f428372
Sergey Kirichenko <spkirich@gmail.com>
2022-10-12 19:02:27 +0300

    Eventually fix task-02.c

--- lab-02/task-02.c
+++ lab-02/task-02.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int main()
 {
-    int child_pid[2];
+    pid_t child_pid[2];
+    int stat;
 
-    for (size_t i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++)
     {
         if ((child_pid[i] = fork()) == -1)
         {
@@ -20,31 +22,33 @@ int main()
             printf("I am %d; my group is %d; my parent is %d.\n",
                 getpid(), getpgrp(), getppid());
 
-            sleep(1);
-
             return 0;
         }
 
         else
         {
-            printf("I am %d; my group is %d; ", getpid(), getpgrp());
-            printf("my child is %d\n", child_pid[i]);
+            printf("I am %d; my group is %d; ",
+                getpid(), getpgrp());
+
+            printf("my child is %d\n",
+                child_pid[i]);
         }
     }
 
-    int stat;
-
-    for (size_t i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++)
     {
         pid_t child = wait(&stat);
 
-        printf("My child %d has finished ", child);
+        printf("My child %d has ", child);
 
         if (WIFEXITED(stat))
-            printf("with return code %d.\n", WEXITSTATUS(stat));
+            printf("finished normally with return code %d.\n", WEXITSTATUS(stat));
 
-        else
-            printf("abnormally.\n");
+        else if (WIFSIGNALED(stat))
+            printf("terminated, received signal %d.\n", WTERMSIG(stat));
+        
+        else if (WIFSTOPPED(stat))
+            printf("stopped, received signal %d.\n", WSTOPSIG(stat));
     }
 
     return 0;
