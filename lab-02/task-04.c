7f2dcf25a5547a029853de7b152e3f8fc5b5af7e
Sergey Kirichenko <spkirich@gmail.com>
2022-10-12 19:23:25 +0300

    Eventually fix task-04.c

--- lab-02/task-04.c
+++ lab-02/task-04.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 int main()
 {
-    int child_pid[2], fd[2];
+    pid_t child_pid[2];
+    int fd[2], stat;
 
     if (pipe(fd) == -1)
     {
@@ -13,7 +15,7 @@ int main()
         exit(1);
     }
 
-    for (size_t i = 0; i < 2; i++)
+    for (int i = 0; i < 2; i++)
     {
         if ((child_pid[i] = fork()) == -1)
         {
@@ -23,12 +25,6 @@ int main()
 
         else if (child_pid[i] == 0)
         {
-            if (close(fd[0]) == -1)
-            {
-                perror("Failed to close");
-                exit(1);
-            }
-
             printf("I am %d; my group is %d; my parent is %d.\n",
                 getpid(), getpgrp(), getppid());
 
@@ -36,7 +32,9 @@ int main()
             {
                 case 0:
                 {
-                    char message[] = "Hello world!";
+                    char message[] = "xxxxx";
+
+                    close(fd[0]);
 
                     if (write(fd[1], message, sizeof(message)) == -1)
                     {
@@ -49,7 +47,9 @@ int main()
 
                 case 1:
                 {
-                    char message[] = "Terve maailma!";
+                    char message[] = "XXXXXXXXXXXXXXX";
+
+                    close(fd[0]);
 
                     if (write(fd[1], message, sizeof(message)) == -1)
                     {
@@ -64,26 +64,17 @@ int main()
 
         else
         {
-            printf("I am %d; my group is %d; ", getpid(), getpgrp());
-            printf("my child is %d\n", child_pid[i]);
-        }
-    }
+            printf("I am %d; my group is %d; ",
+                getpid(), getpgrp());
 
-    if (close(fd[1]) == -1)
-    {
-        perror("Failed to close");
-        exit(1);
+            printf("my child is %d.\n",
+                child_pid[i]);
+        }
     }
 
     char message[16];
 
-    if (read(fd[0], message, sizeof(message)) == -1)
-    {
-        perror("Failed to read");
-        exit(1);
-    }
-
-    printf("%s\n", message);
+    close(fd[1]);
 
     if (read(fd[0], message, sizeof(message)) == -1)
     {
@@ -93,19 +84,28 @@ int main()
 
     printf("%s\n", message);
 
-    int stat;
+    if (read(fd[0], message, sizeof(message)) == -1)
+    {
+        perror("Failed to read");
+        exit(1);
+    }
 
-    for (size_t i = 0; i < 2; i++)
+    printf("%s\n", message);
+
+    for (int i = 0; i < 2; i++)
     {
         pid_t child = wait(&stat);
 
-        printf("My child %d has finished ", child);
+        printf("My child %d has ", child);
 
         if (WIFEXITED(stat))
-            printf("with return code %d.\n", WEXITSTATUS(stat));
+            printf("finished normally with return code %d.\n", WEXITSTATUS(stat));
 
-        else
-            printf("abnormally.\n");
+        else if (WIFSIGNALED(stat))
+            printf("terminated, received signal %d.\n", WTERMSIG(stat));
+        
+        else if (WIFSTOPPED(stat))
+            printf("stopped, received signal %d.\n", WSTOPSIG(stat));
     }
 
     return 0;
