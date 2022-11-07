31d0f62d6f8f95854cbeba6a0ae51d4980ac7c3d
Sergey Kirichenko <spkirich@gmail.com>
2022-11-07 17:09:46 +0300

    Add main function

--- lab-03/main.c
+++ lab-03/main.c
@@ -1,6 +1,6 @@
-#include <asm-generic/errno-base.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <pthread.h>
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,6 +13,8 @@
 
 #define LOCKFILE "/var/run/daemon.pid"
 #define LOCKMODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+
+sigset_t mask;
 
 extern int lockfile(int);
 
@@ -117,4 +119,94 @@ int already_running()
     write(fd, buf, strlen(buf) + 1);
 
     return 0;
+}
+
+void reread()
+{
+    // TODO
+}
+
+void *thr_fn(void *arg)
+{
+    int err, signo;
+
+    for (;;)
+    {
+        err = sigwait(&mask, &signo);
+
+        if (err == -1)
+        {
+            syslog(LOG_ERR, "failed to sigwait");
+            exit(1);
+        }
+
+        switch (signo)
+        {
+        case SIGHUP:
+            syslog(LOG_INFO, "reading config file");
+            reread();
+            break;
+
+        case SIGTERM:
+            syslog(LOG_INFO, "terminating");
+            exit(0);
+
+        default:
+            syslog(LOG_INFO, "caught unexpected signal %d", signo);
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int err;
+    pthread_t tid;
+    char *cmd;
+    struct sigaction sa;
+
+    if ((cmd = strrchr(argv[0], '/')) == NULL)
+        cmd = argv[0];
+
+    else
+        cmd++;
+
+    daemonize(cmd);
+
+    if (already_running())
+    {
+        syslog(LOG_ERR, "daemon is already running");
+        exit(1);
+    }
+
+    sa.sa_handler = SIG_DFL;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    if (sigaction(SIGHUP, &sa, NULL) == -1)
+    {
+        perror("failed to sigaction");
+        exit(1);
+    }
+
+    sigfillset(&mask);
+
+    if ((err = pthread_sigmask(SIG_BLOCK, &mask, NULL)) == -1)
+    {
+        perror("failed to pthread_sigmask");
+        exit(1);
+    }
+
+    err = pthread_create(&tid, NULL, thr_fn, 0);
+
+    if (err == -1)
+    {
+        perror("failed to pthread_create");
+        exit(1);
+    }
+
+    // TODO
+
+    exit(0);
 }
